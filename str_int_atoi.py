0c766b86858f7fd21763749f2ec4cd1e5211b4b9
Neel Desai <neelbhaveshdesai@gmail.com>
2023-08-28 23:58:29 -0700

    github copilot added

---
+++ str_int_atoi.py
@@ -0,0 +1,15 @@
+import re
+class Solution:
+    def myAtoi(self, s: str):
+        
+        
+        return s
+
+if __name__=="__main__":
+    so=Solution()
+    s = "42"
+    print(so.myAtoi(s))
+    s = "   -42"
+    print(so.myAtoi(s))
+    s = "4193 with words"
+    print(so.myAtoi(s))
